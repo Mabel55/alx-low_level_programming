35eb0266fa4a48b8172035fba78af2068706e535
Mabel55 <nasaadanna@gmail.com>
2023-03-23 08:18:59 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include "main.h"
 
 int _isupper(int c);
 int _isdigit(int c);
@@ -15,5 +16,7 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
+
+
 
 #endif /* MAIN_H */
