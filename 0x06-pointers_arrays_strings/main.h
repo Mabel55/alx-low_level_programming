0ac4aaefba49398a67a7f94421ff385c03a69cdb
Mabel55 <nasaadanna@gmail.com>
2023-03-29 05:30:02 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 
 #include <stdio.h>
-
+int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -11,8 +11,8 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *s);
 char *cap_string(char *s);
 char *leet(char *s);
-int _putchar(char c);
-
+char *rot13(char *);
+void print_number(int n);
 
 
 #endif
